9f0a149986deaf2e6f9889d4feca6717305a50b8
gpasztor <pasztorgergo33@gmail.com>
2026-02-04 18:58:24 +0100

    Achieved first triangle rendering

--- src/includes/object.hpp
+++ src/includes/object.hpp
@@ -1,4 +1,3 @@
-#include <GLFW/glfw3.h>
 #include <iostream>
 #include <fstream>
 #include <vector>
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include "includes/object.hpp"
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "includes/object.hpp"
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -8,8 +9,8 @@ int main(int argc, char **argv) {
     };
 
     GLFWwindow* window;
-
-    if (!glfwInit()) {
+    
+    if (glfwInit() != GLFW_TRUE) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     };
@@ -17,14 +18,37 @@ int main(int argc, char **argv) {
     window = glfwCreateWindow(500, 500, "Scop", NULL, NULL);
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    };
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
     Object obj(argv[1]);
 
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
+    float positions[6] = {
+         0.0f,  0.5f,
+         0.5f,  0.0f,
+         -0.5f,  0.0f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
 
