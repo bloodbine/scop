ee0a9672d1c98fff072457df7822ea2d50f1f2da
gpasztor <pasztorgergo33@gmail.com>
2026-05-05 16:37:17 +0200

    Tiny cleanup

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -12,7 +12,7 @@ void Camera::computeViewMatrix() {
     double forwardX = targetX - posX;
     double forwardY = targetY - posY;
     double forwardZ = targetZ - posZ;
-    
+
     // Normalize forward
     double len = sqrt(forwardX*forwardX + forwardY*forwardY + forwardZ*forwardZ);
     forwardX /= len;
@@ -22,33 +22,33 @@ void Camera::computeViewMatrix() {
     double rightX = -forwardZ;
     double rightY = 0.0;
     double rightZ = forwardX;
-    
+
     len = sqrt(rightX*rightX + rightY*rightY + rightZ*rightZ);
     rightX /= len;
     rightY /= len;
     rightZ /= len;
-    
+
     // Up = right × forward
     double upX = rightY * forwardZ - rightZ * forwardY;
     double upY = rightZ * forwardX - rightX * forwardZ;
     double upZ = rightX * forwardY - rightY * forwardX;
-    
+
     // Build view matrix (column-major)
     viewMatrix[0] = rightX;
     viewMatrix[4] = rightY;
     viewMatrix[8]  = rightZ;
     viewMatrix[12] = -(rightX*posX + rightY*posY + rightZ*posZ);
-    
+
     viewMatrix[1] = upX;
     viewMatrix[5] = upY;
     viewMatrix[9]  = upZ;
     viewMatrix[13] = -(upX*posX + upY*posY + upZ*posZ);
-    
+
     viewMatrix[2] = -forwardX;
     viewMatrix[6] = -forwardY;
     viewMatrix[10] = -forwardZ;
     viewMatrix[14] = -(-forwardX*posX + -forwardY*posY + -forwardZ*posZ);
-    
+
     viewMatrix[3] = 0;
     viewMatrix[7] = 0;
     viewMatrix[11] = 0;
@@ -58,14 +58,14 @@ void Camera::computeViewMatrix() {
 void Camera::computeProjectionMatrix() {
     for (int i = 0; i < 16; i++) projectionMatrix[i] = 0.0f;
     projectionMatrix[15] = 1.0f;
-    
+
     float tanHalfFov = tan(fov * 3.14159f / 360.0f);
     projectionMatrix[0] = 1.0f / (aspect * tanHalfFov);
     projectionMatrix[5] = 1.0f / tanHalfFov;
     projectionMatrix[10] = -(farPlane + nearPlane) / (farPlane - nearPlane);
     projectionMatrix[11] = -1.0f;
     projectionMatrix[14] = -(2.0f * farPlane * nearPlane) / (farPlane - nearPlane);
-    
+
     projectionDirty = false;
 }
 
@@ -106,7 +106,7 @@ void Camera::processKeyboard(GLFWwindow* window, float deltaTime) {
         azimuth += rotationSpeed * deltaTime;
         needsUpdate = true;
     }
-    
+
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
         elevation += rotationSpeed * deltaTime;
         if (elevation > 1.48) elevation = 1.48;
